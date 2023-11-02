d1d3e984bdd4341402838289bbbf9a7eda7b85dc
AlimCetin <Alim.cetin37@gmail.com>
2023-11-02 20:40:48 +0300

    Start :)

---
+++ src/main/java/com/alicetin/vkicalculetion/VkiCalculetionApplication.java
@@ -0,0 +1,13 @@
+package com.alicetin.vkicalculetion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkiCalculetionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkiCalculetionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alicetin/vkicalculetion/VkiCalculetionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alicetin.vkicalculetion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkiCalculetionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
